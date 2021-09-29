3697bcd56a7f36a7362b4197d5a03872714049e0
lay <1581133593@qq.com>
2021-09-29 15:04:04 +0800

    feat(router): router

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -3,10 +3,24 @@ const connection = require('../app/database');
 class UserService {
   async create(user) {
     try {
-      const { name, password } = user;
+      const {
+        name,
+        password,
+        realname = null,
+        cellphone = null,
+        departmentId = null,
+        roleId = null,
+      } = user;
 
-      const statement = `INSERT INTO users (name,password) VALUES (?,?);`;
-      const result = await connection.execute(statement, [name, password]);
+      const statement = `INSERT INTO users (name,password ,realname,cellphone , departmentId ,roleId) VALUES (?,?,?,?,?,?);`;
+      const result = await connection.execute(statement, [
+        name,
+        password,
+        realname,
+        cellphone,
+        departmentId,
+        roleId,
+      ]);
       return result[0];
     } catch (error) {
       console.log(error);
